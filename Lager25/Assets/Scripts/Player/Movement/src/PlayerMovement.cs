3c2f1227317812d420a46fd1a5ff7b6526911db5
Oskar Månsson <o92design@gmail.com>
2021-10-18 18:28:41 +0200

    [#3] Player Movement
    1. Fixed Movement calculating object direction on fixed update
    2. Added player rotation
    
    --[Why]--
    1. Since we rotate the object each frame we need to calculate the direction each frame as well.
    2. Rotation is using Input action to get delta value of x and y

--- Lager25/Assets/Scripts/Player/Movement/src/PlayerMovement.cs
+++ Lager25/Assets/Scripts/Player/Movement/src/PlayerMovement.cs
@@ -18,6 +18,9 @@ public class PlayerMovement : MonoBehaviour
 {
   public MovementData m_data;
 
+  private int m_forward;
+  private int m_strafe;
+
   public void Start()
   {
     m_data.CharacterController = GetComponent<CharacterController>();
@@ -26,27 +29,21 @@ public class PlayerMovement : MonoBehaviour
 
   public void FixedUpdate()
   {
+    m_data.Movement = (transform.forward * m_forward + transform.right * m_strafe) * m_data.Speed;
     m_data.CharacterController.SimpleMove(m_data.Movement * Time.deltaTime);
   }
 
-  public void OnMove(InputValue p_value)
+  public void Move(InputAction.CallbackContext p_action)
   {
     // TODO(oskar): Make this branchless bound check - see casey muratori lecture on performance
-    float directionY = p_value.Get<Vector2>().y;
-    float directionX = p_value.Get<Vector2>().x;
-    int directionForward = directionY > 0 ? 1 :
-                           directionY < 0 ? -1 : 
-                           0;
-    int directionStrafe = directionX > 0 ? 1 :
-                          directionX < 0 ? -1 :
-                          0;
+    float directionY = p_action.ReadValue<Vector2>().y;
+    float directionX = p_action.ReadValue<Vector2>().x;
+    m_forward = directionY > 0 ? 1 :
+                directionY < 0 ? -1 :
+                0;
+    m_strafe = directionX > 0 ? 1 :
+               directionX < 0 ? -1 :
+               0;
 
-    Vector3 forward = transform.forward * directionForward;
-    Vector3 strafe = transform.right * directionStrafe;
-
-    m_data.Movement.z = forward.z;
-    m_data.Movement.x = strafe.x;
-    m_data.Movement *= m_data.Speed;
   }
-
 }
