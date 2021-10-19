85d93db08de56c28f2dfd0cfc1fbf38cf09ca245
Oskar Månsson <o92design@gmail.com>
2021-10-19 10:48:40 +0200

    [#6] Shooting
    1. Weapon model placeholder
    2. Fire Input for shooting
    
    --[Why]--
    1. A first step toward implementing shooting is used to see the aim of the player
    2. Input System controls when player fires

---
+++ Lager25/Assets/Scripts/Player/Shooting/src/Shooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public struct ShootingData
+{
+  public bool Shooting;
+}
+
+public class Shooting : MonoBehaviour
+{
+  public ShootingData m_Data;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(m_Data.Shooting)
+    {
+      Debug.Log("Shooting");
+    }
+  }
+
+  public void Fire(InputAction.CallbackContext p_context)
+  {
+    m_Data.Shooting = p_context.performed;
+  }
+
+  public void Fire()
+  {
+    m_Data.Shooting = true;
+  }
+
+  public void StopFire()
+  {
+    m_Data.Shooting = false;
+  }
+}
