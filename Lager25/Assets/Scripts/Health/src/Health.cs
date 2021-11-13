8ce097bf218cb88e15914af65749d3c5a7e6462c
Oskar Månsson <o92design@gmail.com>
2021-11-13 18:48:10 +0100

    [#29] Health System
    1. Change method which returns true if health is zero or below
    
    --[Why]--
    1. We only need to check if health is below zero when we're changing the value and then perform object pooling.

---
+++ Lager25/Assets/Scripts/Health/src/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+  public int Value = 100;
+
+  /// <summary>
+  /// Changes the value of the Health with given parameter
+  /// </summary>
+  /// <param name="p_Value">Value to change Health</param>
+  /// <returns>bool to check if value is below or equal to 0</returns>
+  public bool ChangeHealth(int p_Value)
+  {
+    Value += p_Value;
+
+    return Value <= 0;
+  }
+}
